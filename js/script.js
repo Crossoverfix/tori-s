48641e63cbd54d5a1afb5cb48b0c99ab66777bd8
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2018-12-03 17:27:20 +0300

    v1.2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            items:1,
+            dots:true,
+            dotsEach:true,
+            autoplay:true
+        });
+    });
 })
